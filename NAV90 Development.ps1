375575b7fa6292c4bb5255ea64e6978aeda83ab4
ivomol <molek@webcom.cz>
2016-03-17 16:33:54 +0100

    first NAV90 scripts

---
+++ NAV90 Development.ps1
@@ -0,0 +1,2 @@
+Import-Module 'C:\Program Files (x86)\Microsoft Dynamics NAV\90\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.dll' -DisableNameChecking
+Get-Command -Module Microsoft.Dynamics.Nav.Model.Tools
---
+++ NAV90 Management.ps1
@@ -0,0 +1,2 @@
+Import-Module 'C:\Program Files\Microsoft Dynamics NAV\90\Service\Microsoft.Dynamics.Nav.Management.dll'
+Get-Command -Module Microsoft.Dynamics.Nav.Management
